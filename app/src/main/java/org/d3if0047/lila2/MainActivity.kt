8d63d90cfe218f0d51ca40bf35a5825c81752145
Khalilah Atik Akmal <khalilahatikaakmal@gmail.com>
2025-05-19 02:59:56 +0700

    git commit

--- app/src/main/java/org/d3if0047/lila2/MainActivity.kt
+++ app/src/main/java/org/d3if0047/lila2/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "lila2 $name!",
+        text = "lila21 $name!",
         modifier = modifier
     )
 }
